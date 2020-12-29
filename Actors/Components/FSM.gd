2b84b49606a79252323ad2b3e8a89de274f094a7
srcarr1515 <srcarr@gmail.com>
2020-12-29 08:33:46 -0500

    fix gameover bug

--- Actors/Components/FSM.gd
+++ Actors/Components/FSM.gd
@@ -35,11 +35,18 @@ func death(delta):
 			dock.char_status = "death"
 	### I don't have a death animation, let's just hide until then.
 	parent.hide()
+	var player_chars = get_tree().get_nodes_in_group("player_char")
+	var death_ct = 0
+	for pc in player_chars:
+		if pc.state.current == "death":
+			death_ct += 1
+	if death_ct >= 3:
+		GameState.set_death_ct(death_ct)
 	yield(get_tree().create_timer(0.1), "timeout")
-	GameState.death_ct += 1
+#	GameState.death_ct += 1
 
 func revive(delta):
-	GameState.death_ct -= 1
+#	GameState.death_ct -= 1
 	## You are not dead, get back to work.
 	parent.detect.radius.set_disabled(false)
 	var skill_docks = get_tree().get_nodes_in_group("skill_dock")
